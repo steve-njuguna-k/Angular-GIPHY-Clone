ec931330fc5229a03174f4107afc824bbb3729cd
Steve Njuguna <stevenjuguna93@gmail.com>
2021-11-24 21:45:31 +0300

    Added Observables to all http methods

--- src/app/services/giphy.service.ts
+++ src/app/services/giphy.service.ts
@@ -1,6 +1,8 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
+import { Giphs } from '../interfaces/giphs';
 
 @Injectable({
   providedIn: 'root'
@@ -12,19 +14,19 @@ export class GiphyService {
 
   constructor(private http: HttpClient) { }
 
-  getGifs(){
-    return this.http.get(`https://api.giphy.com/v1/gifs/search?api_key=${this.apiKey}&q=${this.query}&limit=50&offset=0&rating=G&lang=en`);
+  getGifs(): Observable<Giphs[]>{
+    return this.http.get<Giphs[]>(`https://api.giphy.com/v1/gifs/search?api_key=${this.apiKey}&q=${this.query}&limit=50&offset=0&rating=G&lang=en`);
   }
 
-  trendingGifs(){
-    return this.http.get(`https://api.giphy.com/v1/gifs/trending?api_key=${this.apiKey}&limit=50&rating=G`);
+  trendingGifs(): Observable<Giphs[]>{
+    return this.http.get<Giphs[]>(`https://api.giphy.com/v1/gifs/trending?api_key=${this.apiKey}&limit=50&rating=G`);
   }
   
-  randomGif(){
-    return this.http.get(`https://api.giphy.com/v1/gifs/random?api_key=${this.apiKey}&tag=&rating=G`);
+  randomGif(): Observable<Giphs[]>{
+    return this.http.get<Giphs[]>(`https://api.giphy.com/v1/gifs/random?api_key=${this.apiKey}&tag=&rating=G`);
   }
 
-  searchGis(query:string){
+  searchGif(query:string){
     this.query = query;
   }
 
