c15109cf0eeec4592c6d0d6560133a311ab2596e
Steve Njuguna <stevenjuguna93@gmail.com>
2021-11-24 21:26:09 +0300

    Added Giphy endpoints to service

--- src/app/services/giphy.service.ts
+++ src/app/services/giphy.service.ts
@@ -1,9 +1,31 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class GiphyService {
 
-  constructor() { }
+  private apiKey = environment.apiKey;
+  private query!: string;
+
+  constructor(private http: HttpClient) { }
+
+  getGifs(){
+    return this.http.get(`https://api.giphy.com/v1/gifs/search?api_key=${this.apiKey}&q=${this.query}&limit=50&offset=0&rating=G&lang=en`);
+  }
+
+  trendingGifs(){
+    return this.http.get(`https://api.giphy.com/v1/gifs/trending?api_key=${this.apiKey}&limit=50&rating=G`);
+  }
+  
+  randomGif(){
+    return this.http.get(`https://api.giphy.com/v1/gifs/random?api_key=${this.apiKey}&tag=&rating=G`);
+  }
+
+  searchGis(query:string){
+    this.query = query;
+  }
+
 }
