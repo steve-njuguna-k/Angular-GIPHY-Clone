0afc4d0ce688e5b0d36c49a8bb31c9cada5ec8fd
Steve Njuguna <stevenjuguna93@gmail.com>
2021-11-25 22:42:43 +0300

    Added routes to links in navbar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CategoriesComponent } from './components/categories/categories.component';
+import { GiphyDisplayComponent } from './components/giphy-display/giphy-display.component';
+import { RandomComponent } from './components/random/random.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: GiphyDisplayComponent},
+  {path: 'categories', component: CategoriesComponent},
+  {path: 'random', component: RandomComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
