fd5ba9ecceb74446faec607737063360188631f5
Steve Njuguna <stevenjuguna93@gmail.com>
2021-11-26 16:42:42 +0300

    Added spliced trending gifs & gif categories

--- src/app/components/giphy-display/giphy-display.component.ts
+++ src/app/components/giphy-display/giphy-display.component.ts
@@ -8,11 +8,34 @@ import { GiphyService } from 'src/app/services/giphy.service';
 })
 export class GiphyDisplayComponent implements OnInit {
 
+  allTrendingGifs:any[] = [];
+  allGifCategories:any[] = [];
+
   constructor(private service: GiphyService) { }
 
   ngOnInit(): void {
-
+    this.TrendingGifs();
+    this.Categories();
+    this.service.allGifs()
+      .subscribe((response: any) => {
+        this.allTrendingGifs = response;
+        console.log(this.allTrendingGifs)
+      }
+    );
+    this.service.allCategories()
+      .subscribe((response: any) => {
+        this.allGifCategories = response;
+        console.log(this.allGifCategories)
+      }
+    );
   }
 
+  TrendingGifs(){
+    this.service.getTrendingGifs();
+  }
+
+  Categories(){
+    this.service.getCategories();
+  }
 
 }
